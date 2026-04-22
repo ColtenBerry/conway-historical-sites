c2275d0d8a8097ec04bd28da53452e0e387af3fd
Colten Berry <berrycj@hendrix.edu>
2026-04-22 15:18:49 -0500

    Updated Profile button

--- faulkner_footsteps/lib/widgets/profile_button.dart
+++ faulkner_footsteps/lib/widgets/profile_button.dart
@@ -1,12 +1,14 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:faulkner_footsteps/app_state.dart';
 import 'package:flutter/material.dart';
 import 'package:faulkner_footsteps/app_router.dart';
+import 'package:provider/provider.dart';
 
 class ProfileButton extends StatelessWidget {
   const ProfileButton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final appState = Provider.of<ApplicationState>(context, listen: false);
     return IconButton(
       icon: const Icon(
         Icons.person,
@@ -14,8 +16,7 @@ class ProfileButton extends StatelessWidget {
       ),
       onPressed: () {
         print("attempting to get user");
-        final user = FirebaseAuth.instance.currentUser;
-        if (user == null) {
+        if (!appState.loggedIn) {
           print("user is logged out");
           AppRouter.navigateTo(context, AppRouter.loginPage);
         } else {
